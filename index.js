ab13d3aae4aab4d8229e386f9759f272a7d7258e
CARLOS CORDOVA <cordovamanuel22@icloud.com>
2023-01-14 11:58:11 -0800

    2

--- index.js
+++ index.js
@@ -1,48 +1,202 @@
+//! lo que quiero 
+
+//? 1. requerir express 
 const express = require('express');
-const app = express();
+// const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
 
-app.use(express.json()); //! when using body post
+const { request, response } = require('express');
 
-//? get function
-app.get('/', (req, res) => {
-  res.send('api de prueva de carlos')
+//! valores iniciales o por defecto 
+dotenv.config();
+
+const app = express(); //? inicialisar server 
+const port = process.env.SERVER_PORT; //? sets port 
+/* 
+nombre,
+precio,
+descripcion,
+categoria,
+ID
+*/
+
+const dishesList = [
+  {
+    name: " Pupusas ",
+    price: 2.00,
+    description: " tortia fill with meat,cheese & beans ",
+    type: " not vegan ",
+    id: 1,
+  },
+  {
+    name: " burger ",
+    price: 4.00,
+    description: " bon with lettuce,tomato, steak patty & cheese ",
+    type: "not vegan",
+    id: 2,
+  },
+
+]; //? array para una lista de platillos
+var lastId = 2; //? contador de id 
+
+const findById = (id) => {
+  //  var id = request.params.identificador;
+  //  array.forEach(element => {
+  //! no usar porque no se puede detener el proceso
+  //  });
+  var found = null;
+  for (const platillo of dishesList) {
+    if (platillo["id"] == id) {
+      found = platillo;
+      break;
+    }
+  }
+  return found;
+};
+
+
+const findPositionById = (id) => {
+  var index = 0;
+  var found = null;
+  for (const platillo of dishesList) {
+    if (platillo["id"] == id) {
+      found = index;
+      break;
+    }
+    index++
+  }
+  if (found != null)
+    return found
+  return -1;
+};
+
+
+
+//! proceso o estructura 
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+//? health check 
+app.get("/status", (req, res) => {
+  res.status(200)
+    .json({
+      "done": "true",
+      "api": {
+        "name": "express-api-clase",
+        "version": "v1",
+        "owner": "carlos cordova",
+        "dev": "carlos dev"
+      },
+      "services": {
+
+        "database": "error de conexion"
+
+      }
+
+    });
 });
 
-//? add user or submit info to server con query method 
-app.post('/useradded', (req, res) => {
-  console.log(req.query);
-  res.end()
+/* //! protocolo HTTP
+* que recurso  - PLATILLO
+* que metodo - GET
+* que datos de entrada requiere? -  N\A
+* que debe responder ? lista con todos los dishes del menu
+*/
 
-
+//? GET  ALL PLATILLOS
+app.get("/platillos", (request, response) => {
+  response.json({
+    quantity: dishesList.length,
+    items: dishesList
+  });
 });
 
-//? add user or submit info to server con post  pararms method 
-app.post('/useradded2/:nombre/:edad/', (req, res) => {
-  console.log(req.params);
-  res.end();
+/* //! protocolo HTTP
+* que recurso  - PLATILLO
+* que metodo - GET
+* que datos de entrada requiere? -  id (param or path)
+* que debe responder ? obtener un platillo por id 
+*/
+
+//? GET PLATILLOS:/id
+app.get("/platillos/:identificador", (request, response) => {
+  var id = request.params.identificador;
+  //  array.forEach(element => {
+  //! no usar porque no se puede detener el proceso
+  //  });
+  var found = null;
+  for (const platillo of dishesList) {
+    if (platillo["id"] == id) {
+      found = platillo;
+      break;
+    }
+  }
+  response.json({
+    done: !(found == null),
+    item: found
+  });
 });
 
-//? add user or submit info to server con post  body method 
-app.post('/useradded3', (req, res) => {
-  console.log(req.body);
-  res.end();
+
+/* //! protocolo HTTP
+* que recurso  - PLATILLO
+* que metodo - POST
+* que datos de entrada requiere? -  /
+* que debe responder ? AGREGAR UN PLATILLO 
+*/
+//? post  /PLATILLOS/ 
+app.post('/platillos/', (request, response) => {
+  lastId++;
+  let info = request.body;
+  nuevoPlatillo = {
+    name: info.name,
+    price: info.price,
+    description: info.description,
+    type: info.type,
+    id: lastId,
+
+  };
+  dishesList.push(nuevoPlatillo);
+  response.json({
+    done: true,
+    item: nuevoPlatillo,
+  });
+});
+
+app.put("/platillos/:id", (req, res) => {
+  var platillo = findById(req.params.id);
+  if (platillo) {
+    platillo["price"] = req.body.price;
+  };
+  response.json({
+    done: !(found == null),
+    id: req.params.id,
+    data: req.body
+  });
+});
+app.delete("/platillos/:id", (req, res) => {
+  var position = findPositionById(req.params.id);
+  var deleted = false;
+  var positionIndex = dishesList.findIndex(platillo => platillo.id == req.params.id);
+  if (position != -1 ){
+    dishesList.splice(position,1);
+    deleted = true;
+  };
+  res.json({
+    done: deleted,
+    id:req.params.id
+  })
+});
+
+//!consumo
+app.listen(port, () => {
+  console.log(`runing server on http://localhost:${port}`);
 });
 
 
-
-
-
-
-
-
-
-
-app.listen('4000', () => {
-  console.log("el server esta corriento en el puerto 4000");
-});
-
-
-
+//* notas
+// request.query //? GET /PLATILLO?CATEGORIA=SNANCK -> QUERY PARAM
+// request.params //? GET PLATILLO/2   -> PATH PARAM
+// request.body //? post /platillo/ ....... -> data param
 
 
 
